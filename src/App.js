399e7b6931c7994e5ec39fa1c9d45c7b0b271bd7
Dawncute <lethibinhminh1307@gmail.com>
2024-09-22 22:20:07 +0700

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import Header from './components/header/header'
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
-    <Header></Header>
+    <div className='App'>
+      <Header></Header>
+      <Hero></Hero>
+    </div>
   );
 }
 
